d5277ae2a19ef35ed6c1e43d4ec95592e0c75742
Dezmone Musgrove <Dezmone.Musgrove@gmail.com>
2021-10-25 12:39:50 -0400

    gettin right with Shanshan

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,48 @@
+public class ControlFlowExercises {
+    public static void main(String[] args) {
+//        boolean isSunny = false;
+//        boolean hasUmbrella = true;
+//
+//        if (isSunny) {
+//            System.out.println("yay");
+//        } else if (!isSunny && hasUmbrella) {
+//            System.out.println("might be a bad day");
+//        } else {
+//            System.out.println("go look at the weather");
+//        }
+
+//        int caseSwitch = 3;
+//
+//        switch (caseSwitch){
+//            case 1:
+//                System.out.println("Case 1");
+//                break;
+//            case 2:
+//                System.out.println("Case 2");
+//                break;
+//            default:
+//                System.out.println("Default case");
+//        }
+
+//        for (int i = 1; i < 100; i++){
+//            System.out.println(i);
+//            if (i == 15){
+//                break;
+//            }
+//        }
+
+        int i = 5;
+
+        while (i <= 15) {
+            System.out.print(i + " ");
+            i++;
+        }
+
+        int x = 0;
+        do {
+            System.out.println(i % 2);
+
+        } while (x <=100);
+
+    }
+}
