d305058ef4077eb2e11046f21f8c01efe8aa9882
Dezmone Musgrove <Dezmone.Musgrove@gmail.com>
2021-10-25 10:31:19 -0400

    syn, types, & vars review

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class ConsoleExercises {
     public static void main(String[] args){
-//        String name = "Dezmone";
-////        System.out.printf("Hello there, %s. Nice to see you.\n", name);
-//
-//        String greeting = "Salutations";
-////        String name = "codeup";
-//        System.out.printf("%s, %s!", greeting, name);
-//
+        String name = "Dezmone";
+        System.out.printf("Hello there, %s. Nice to see you.\n", name);
+
+        String greeting = "Salutations";
+//        String name = "codeup";
+        System.out.printf("%s, %s!", greeting, name);
+
         Scanner scanner = new Scanner(System.in);
 //
 //        System.out.print("Enter something: ");
@@ -24,7 +24,7 @@ public class ConsoleExercises {
 //        The value of pi is approximately 3.14.
 //        Don't change the value of the variable; instead, reference one of the links above and use System.out.format to accomplish this.
 
-        System.out.printf("The value of pi is approximately %s" + "/n", pi);
+        System.out.printf("The value of pi is approximately %.2f" + "/n", pi);
 
 
 //        Explore the Scanner Class.
@@ -83,19 +83,19 @@ public class ConsoleExercises {
 //                The area of a rectangle is equal to the length times the width, and the perimeter of a rectangle is equal to 2 times the length plus 2 times the width.
 
         System.out.println("The area is: " + (userLength * userWidth) + " sqft.");
-        System.out.println("The perimeter is: " + (userLength * 4) + " ft.") ;
+        System.out.println("The perimeter is: " + (userLength * 2) + (userWidth * 2) + " ft.") ;
 
 //                Bonuses
 //        In your perimeter/area calculator: - Accept decimal entries. - Calculate the volume of the rooms in addition to the area and perimeter.
-
-        float userHeight = scanner.nextFloat();
-        float volume = userLength * userWidth + userHeight;
-        System.out.println("The Volume is: " + volume + "sqft.");
+//
+//        float userHeight = scanner.nextFloat();
+//        float volume = userLength * userWidth + userHeight;
+//        System.out.println("The Volume is: " + volume + "sqft.");
 
 //        The Scanner class can be told specifically what characters or pattern separates tokens in a piece of input with the useDelimiter method. Add the following line of code to your application after you have created a scanner (assuming the variable holding the Scanner is named scanner):
 //
 
-        System.out.format("%-10.3f%n", volume);
+        System.out.format("%+010f dez", -123.1234);
 
 //        scanner.useDelimiter("\n");
 //        How does this change the way your program operates?
